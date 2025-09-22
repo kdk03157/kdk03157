1d1030763933479a989143d8796146cf5676639a
kdk03157 <kdk03157@naver.com>
2025-09-22 11:39:45 +0900

    250922 삼성꿈장학재단

--- sdream/sdream/js/main.js
+++ sdream/sdream/js/main.js
@@ -25,4 +25,53 @@ $(document).ready(function(){
         $(this).hide()
         $('.visual .btn_wrap .btn_play').show()
     })
+
+    const webzine_swiper = new Swiper('.webzine .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 640px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+    });
+
+    $('footer .top').on('click', function(){
+        // console.log('top 클릭')
+        $('html, body').animate({
+            scrollTop : 0
+        }, 500)
+    })
+
+
+    $('header').on('mouseenter',function(){
+        $(this).addClass('fixed')
+        console.log('header에 오버함')
+    })
+    $('header').on('mouseleave', function(){
+        console.log('올렸다가 내림')
+        $(this).removeClass('fixed')
+        if(scrolling <=0){
+            console.group('스크롤 값은 0이거나 0보다 작다')
+        }
+    })
+
+    //문서가 로딩괸 이후 단 1번 실행
+    let scrolling = $(window).scrollTop()
+    console.log(scrolling)
+    $(window).scroll(function(){
+        //스크롤 할때마다 1번 실행
+        // console.log(scrolling)
+        scrolling = $(window).scrollTop()
+        //스크롤값이 0보다 크면 header에 fixed 클래스를 추가함
+        if(scrolling > 0){
+            console.log('0보다')
+            $('header').addClass('fixed')
+        }else{
+            console.log('0이다')
+            $('header').removeClass('fixed')
+        }
+    })
+
 })//맨끝
